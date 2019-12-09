1bb5aa6b8b100648ee901220747fbde6c66418e1
kelvinsekx <kelvinsekx@gmail.com>
2019-12-09 04:35:46 -1200

    first changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                  Kelvinsekx
                 </div>
 
                 <div class="links">
